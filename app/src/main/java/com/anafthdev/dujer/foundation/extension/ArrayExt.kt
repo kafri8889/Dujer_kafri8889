5f344ab40a8b34c1e2c9f8b34a050277294ade57
kafri8889 <kafri8889@gmail.com>
2022-04-30 11:49:54 +0700

    feat: new function

--- app/src/main/java/com/anafthdev/dujer/foundation/extension/ArrayExt.kt
+++ app/src/main/java/com/anafthdev/dujer/foundation/extension/ArrayExt.kt
@@ -1,5 +1,15 @@
 package com.anafthdev.dujer.foundation.extension
 
+/**
+ * Returns a list element from given collection
+ * @author kafri8889
+ */
+fun <T, U> Array<T>.getBy(selector: (T) -> U): List<U> {
+	val result = ArrayList<U>()
+	for (v in this) { result.add(selector(v)) }
+	return result
+}
+
 fun <T> Array<T>.get(predicate: (T) -> Boolean): T? {
 	this.forEach {
 		if (predicate(it)) return it
