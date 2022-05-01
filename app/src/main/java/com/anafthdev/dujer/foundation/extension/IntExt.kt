42f08ff5b9f546aa8f697a264fbd5def473f2d7d
kafri8889 <kafri8889@gmail.com>
2022-05-01 23:00:32 +0700

    feat: new function

--- app/src/main/java/com/anafthdev/dujer/foundation/extension/IntExt.kt
+++ app/src/main/java/com/anafthdev/dujer/foundation/extension/IntExt.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
+import kotlin.math.roundToInt
 
 fun Int.isMinus(): Boolean = this < 0
 
@@ -19,3 +20,18 @@ fun Int.toDp(): Dp = with(LocalDensity.current) { this@toDp.toDp() }
  * Convert an Int pixel value to Dp.
  */
 fun Int.toDp(density: Density): Dp = with(density) { this@toDp.toDp() }
+
+/**
+ * convert to percent
+ *
+ * 12 percentOf 5 = 41.67%
+ *
+ * 14 percentOf 4 = 28.57%
+ */
+fun Int.percentOf(value: Float): Float {
+	return value.div(this).times(100)
+}
+
+fun Int.roundPercentOf(value: Float): Int {
+	return value.div(this).times(100).roundToInt()
+}
