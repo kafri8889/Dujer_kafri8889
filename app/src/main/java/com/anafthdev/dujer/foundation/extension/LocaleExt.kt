b2b4ab6a6fe3451c8159099c6b7217e190d297f3
kafri8889 <kafri8889@gmail.com>
2022-05-12 21:19:07 +0700

    feat: new extension

---
+++ app/src/main/java/com/anafthdev/dujer/foundation/extension/LocaleExt.kt
@@ -0,0 +1,6 @@
+package com.anafthdev.dujer.foundation.extension
+
+import android.content.res.Resources
+import java.util.*
+
+val deviceLocale: Locale = Resources.getSystem().configuration.locales[0]
