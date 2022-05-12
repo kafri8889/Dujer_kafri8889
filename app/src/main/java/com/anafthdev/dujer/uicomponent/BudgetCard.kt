cebe28f2cc9cfbf64fe41b9b44b59db6f15c056a
kafri8889 <kafri8889@gmail.com>
2022-05-12 14:34:08 +0700

    refactor: change background color
    color improvement for light and dark theme

--- app/src/main/java/com/anafthdev/dujer/uicomponent/BudgetCard.kt
+++ app/src/main/java/com/anafthdev/dujer/uicomponent/BudgetCard.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,15 +17,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.anafthdev.dujer.R
+import com.anafthdev.dujer.foundation.extension.isLightTheme
 import com.anafthdev.dujer.foundation.uiextension.horizontalScroll
+import com.anafthdev.dujer.foundation.uimode.data.LocalUiMode
 import com.anafthdev.dujer.foundation.window.dpScaled
 import com.anafthdev.dujer.foundation.window.spScaled
 import com.anafthdev.dujer.model.LocalCurrency
-import com.anafthdev.dujer.ui.dashboard.component.BudgetProgressIndicator
-import com.anafthdev.dujer.ui.theme.Typography
-import com.anafthdev.dujer.ui.theme.balance_card_background
-import com.anafthdev.dujer.ui.theme.full_shape
-import com.anafthdev.dujer.ui.theme.large_shape
+import com.anafthdev.dujer.ui.theme.*
 import com.anafthdev.dujer.util.AppUtil
 import com.anafthdev.dujer.util.CurrencyFormatter
 import java.util.*
@@ -40,6 +36,8 @@ fun BudgetCard(
 	modifier: Modifier = Modifier
 ) {
 	
+	val uiMode = LocalUiMode.current
+	
 	val indicatorProgress = try {
 		(totalExpense / totalIncome).toFloat()
 	} catch (e: Exception) { 0f }
@@ -47,10 +45,10 @@ fun BudgetCard(
 	Card(
 		shape = large_shape,
 		elevation = CardDefaults.cardElevation(
-			defaultElevation = 4.dp
+			defaultElevation = 0.dp
 		),
 		colors = CardDefaults.cardColors(
-			containerColor = balance_card_background
+			containerColor = MaterialTheme.colorScheme.tertiaryContainer
 		),
 		onClick = {},
 		modifier = modifier
@@ -60,47 +58,52 @@ fun BudgetCard(
 			modifier = Modifier
 				.padding(16.dpScaled)
 		) {
-			Row(
-				verticalAlignment = Alignment.CenterVertically,
-				modifier = Modifier
-					.fillMaxWidth()
+			CompositionLocalProvider(
+				LocalContentColor provides if (uiMode.isLightTheme()) black02 else black09
 			) {
-				Text(
-					text = stringResource(
-						id = R.string.budget_for,
-						AppUtil.longMonths[Calendar.getInstance()[Calendar.MONTH]]
-					),
-					style = Typography.bodyMedium.copy(
-						color = Color.White,
-						fontWeight = FontWeight.SemiBold,
-						fontSize = Typography.bodyMedium.fontSize.spScaled
-					),
+				Row(
+					verticalAlignment = Alignment.CenterVertically,
 					modifier = Modifier
-						.weight(0.46f)
-				)
-				
-				Text(
-					textAlign = TextAlign.End,
-					text = CurrencyFormatter.format(
-						locale = AppUtil.deviceLocale,
-						amount = totalIncome - totalExpense,
-						currencyCode = LocalCurrency.current.countryCode
-					),
-					style = Typography.titleMedium.copy(
-						color = Color.White,
-						fontWeight = FontWeight.Bold,
-						fontSize = Typography.titleMedium.fontSize.spScaled
-					),
-					modifier = Modifier
-						.weight(0.54f)
-						.horizontalScroll(
-							state = rememberScrollState(),
-							autoRestart = true
-						)
-				)
+						.fillMaxWidth()
+				) {
+					Text(
+						text = stringResource(
+							id = R.string.budget_for,
+							AppUtil.longMonths[Calendar.getInstance()[Calendar.MONTH]]
+						),
+						style = Typography.bodyMedium.copy(
+							fontWeight = FontWeight.SemiBold,
+							fontSize = Typography.bodyMedium.fontSize.spScaled
+						),
+						modifier = Modifier
+							.weight(0.46f)
+					)
+					
+					Text(
+						textAlign = TextAlign.End,
+						text = CurrencyFormatter.format(
+							locale = AppUtil.deviceLocale,
+							amount = totalIncome - totalExpense,
+							currencyCode = LocalCurrency.current.countryCode
+						),
+						style = Typography.titleMedium.copy(
+							fontWeight = FontWeight.Bold,
+							fontSize = Typography.titleMedium.fontSize.spScaled
+						),
+						modifier = Modifier
+							.weight(0.54f)
+							.horizontalScroll(
+								state = rememberScrollState(),
+								autoRestart = true
+							)
+					)
+				}
 			}
 			
 			BudgetProgressIndicator(
+				indicatorTrackColor = if (uiMode.isLightTheme()) Color.Black.copy(alpha = 0.08f)
+						else Color.White.copy(alpha = 0.36f),
+				stepColor = MaterialTheme.colorScheme.tertiaryContainer,
 				progress = indicatorProgress,
 				modifier = Modifier
 					.fillMaxWidth()
