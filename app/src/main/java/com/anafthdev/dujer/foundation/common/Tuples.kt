f9fe1df6520a9c5f6e1c756397497ab3f84941e0
kafri8889 <kafri8889@gmail.com>
2022-05-22 21:13:47 +0700

    feat: new tuples (Quad & Quint)

---
+++ app/src/main/java/com/anafthdev/dujer/foundation/common/Tuples.kt
@@ -0,0 +1,42 @@
+package com.anafthdev.dujer.foundation.common
+
+import java.io.Serializable
+
+data class Quad<out A, out B, out C, out D>(
+	val first: A,
+	val second: B,
+	val third: C,
+	val fourth: D
+) : Serializable {
+	
+	/**
+	 * Returns string representation of the [Quad] including its
+	 * [first], [second], [third] and [fourth] values.
+	 */
+	override fun toString(): String = "($first, $second, $third, $fourth)"
+}
+
+data class Quint<out A, out B, out C, out D, E>(
+	val first: A,
+	val second: B,
+	val third: C,
+	val fourth: D,
+	val fifth: E
+) : Serializable {
+	
+	/**
+	 * Returns string representation of the [Quint] including its
+	 * [first], [second], [third], [fourth] and [fifth] values.
+	 */
+	override fun toString(): String = "($first, $second, $third, $fourth, $fifth)"
+}
+
+/**
+ * Converts this quad into a list.
+ */
+fun <T> Quad<T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth)
+
+/**
+ * Converts this quad into a list.
+ */
+fun <T> Quint<T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth)
