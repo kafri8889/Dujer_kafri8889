15aede62967371d97f4439095bf0d74af3a1a38c
kafri8889 <kafri8889@gmail.com>
2022-06-05 20:45:13 +0700

    refactor: using `DelayManager` instead of `EventTimerCountdown`

--- app/src/main/java/com/anafthdev/dujer/foundation/uiextension/ModifierExt.kt
+++ app/src/main/java/com/anafthdev/dujer/foundation/uiextension/ModifierExt.kt
@@ -3,6 +3,7 @@ package com.anafthdev.dujer.foundation.uiextension
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -10,9 +11,13 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import com.anafthdev.dujer.foundation.common.DelayManager
 import com.anafthdev.dujer.foundation.common.EventCountdownTimer
 import kotlinx.coroutines.launch
 
+/**
+ * use [aspectRatio]
+ */
 fun Modifier.sizeBasedWidth(
 	enlargement: Float = 1f
 ): Modifier = composed {
@@ -37,14 +42,20 @@ fun Modifier.horizontalScroll(
 	
 	val scope = rememberCoroutineScope()
 	
-	val eventCountdownTimer by remember { mutableStateOf(EventCountdownTimer()) }
+	val delayManager by remember {
+		mutableStateOf(
+			DelayManager(),
+			referentialEqualityPolicy()
+		)
+	}
+	
+	val isTimerRunning by delayManager.isRunning.collectAsState()
 	
 	if (autoRestart) {
 		DisposableEffect(state.isScrollInProgress and (state.value != 0)) {
-			if (!eventCountdownTimer.isTimerRunning) {
-				eventCountdownTimer.startTimer(
-					millisInFuture = autoRestartDelay,
-					onTick = {},
+			if (!isTimerRunning) {
+				delayManager.delay(
+					timeInMillis = autoRestartDelay,
 					onFinish = {
 						scope.launch {
 							state.animateScrollTo(
