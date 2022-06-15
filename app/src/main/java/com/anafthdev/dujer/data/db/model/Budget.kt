0a8a685edbd6b0a2e9bc737877628ba9ee405fb9
kafri8889 <kafri8889@gmail.com>
2022-06-15 18:02:12 +0700

    refactor: delete `name` and add `tint`

--- app/src/main/java/com/anafthdev/dujer/data/db/model/Budget.kt
+++ app/src/main/java/com/anafthdev/dujer/data/db/model/Budget.kt
@@ -3,12 +3,25 @@ package com.anafthdev.dujer.data.db.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.anafthdev.dujer.model.CategoryTint
 
 @Entity(tableName = "budget_table")
 data class Budget(
 	@PrimaryKey(autoGenerate = false) val id: Int,
-	@ColumnInfo(name = "name") val name: String,
 	@ColumnInfo(name = "max") val max: Double,
+	@ColumnInfo(name = "tint") val tint: CategoryTint,
 	@ColumnInfo(name = "category") val category: Category,
 	@ColumnInfo(name = "expenseID") val expenseID: List<Int>,
-)
+) {
+	
+	companion object {
+		val defalut = Budget(
+			id = -1,
+			max = 0.0,
+			tint = CategoryTint.tint_1,
+			category = Category.default,
+			expenseID = emptyList()
+		)
+	}
+	
+}
