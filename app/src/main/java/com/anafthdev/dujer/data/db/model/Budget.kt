4ef7de02768dec50552b9a084a0f46bc3a0e7826
kafri8889 <kafri8889@gmail.com>
2022-06-16 11:09:29 +0700

    refactor: remove `tint` param, use tint from `Category`

--- app/src/main/java/com/anafthdev/dujer/data/db/model/Budget.kt
+++ app/src/main/java/com/anafthdev/dujer/data/db/model/Budget.kt
@@ -3,13 +3,11 @@ package com.anafthdev.dujer.data.db.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.anafthdev.dujer.model.CategoryTint
 
 @Entity(tableName = "budget_table")
 data class Budget(
 	@PrimaryKey(autoGenerate = false) val id: Int,
 	@ColumnInfo(name = "max") val max: Double,
-	@ColumnInfo(name = "tint") val tint: CategoryTint,
 	@ColumnInfo(name = "category") val category: Category,
 	@ColumnInfo(name = "expenseID") val expenseID: List<Int>,
 ) {
@@ -18,7 +16,6 @@ data class Budget(
 		val defalut = Budget(
 			id = -1,
 			max = 0.0,
-			tint = CategoryTint.tint_1,
 			category = Category.default,
 			expenseID = emptyList()
 		)
