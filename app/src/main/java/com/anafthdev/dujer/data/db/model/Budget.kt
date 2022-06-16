d9a974a9b5d294f26c71e9d35dc0df92c7be422b
kafri8889 <kafri8889@gmail.com>
2022-06-16 15:04:58 +0700

    refactor: remove `expenseID` from `Budget`
    to get it, better filter manually

--- app/src/main/java/com/anafthdev/dujer/data/db/model/Budget.kt
+++ app/src/main/java/com/anafthdev/dujer/data/db/model/Budget.kt
@@ -9,15 +9,13 @@ data class Budget(
 	@PrimaryKey(autoGenerate = false) val id: Int,
 	@ColumnInfo(name = "max") val max: Double,
 	@ColumnInfo(name = "category") val category: Category,
-	@ColumnInfo(name = "expenseID") val expenseID: List<Int>,
 ) {
 	
 	companion object {
 		val defalut = Budget(
 			id = -1,
 			max = 0.0,
-			category = Category.default,
-			expenseID = emptyList()
+			category = Category.default
 		)
 	}
 	
