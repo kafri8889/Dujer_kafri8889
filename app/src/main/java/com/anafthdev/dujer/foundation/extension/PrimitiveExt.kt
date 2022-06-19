4379113e92143c1a162d278a97d8f1e93c7bb4e1
kafri8889 <kafri8889@gmail.com>
2022-06-19 13:47:35 +0700

    refactor: move to `PrimitiveExt` and add new extension

--- app/src/main/java/com/anafthdev/dujer/foundation/extension/PrimitiveExt.kt
+++ app/src/main/java/com/anafthdev/dujer/foundation/extension/PrimitiveExt.kt
@@ -42,6 +42,72 @@ fun Double.roundToInt(onNaN: (Double) -> Int): Int {
 
 
 /**
+ * convert to percent
+ *
+ * 12.5 percentOf 5 = 40.0%
+ *
+ * 14.6 percentOf 4.2 = 28.7671%
+ */
+fun Float.percentOf(value: Float, ifNaN: (() -> Float)? = null): Float {
+	val percent = value.div(this).times(100)
+	return if ((ifNaN != null) and percent.isNaN()) ifNaN!!() else percent
+}
+
+fun Float.percentOf(value: Int, ifNaN: (() -> Float)? = null): Float {
+	val percent = value.div(this).times(100)
+	return if ((ifNaN != null) and percent.isNaN()) ifNaN!!() else percent
+}
+
+fun Float.roundPercentOf(value: Float): Int {
+	return value.div(this).times(100).roundToInt()
+}
+
+fun Float.roundPercentOf(value: Int): Int {
+	return value.div(this).times(100).roundToInt()
+}
+
+/**
+ * convert to percent
+ *
+ * 12 percentOf 5 = 41.67%
+ *
+ * 14 percentOf 4 = 28.57%
+ */
+fun Int.percentOf(value: Float): Float {
+	return value.div(this).times(100)
+}
+
+fun Int.roundPercentOf(value: Float): Int {
+	return value.div(this).times(100).roundToInt()
+}
+
+/**
+ * convert to percent
+ *
+ * 12.5 percentOf 5 = 40.0%
+ *
+ * 14.6 percentOf 4.2 = 28.7671%
+ */
+fun Double.percentOf(value: Double, ifNaN: (() -> Double)? = null): Double {
+	val percent = value.div(this).times(100)
+	return if ((ifNaN != null) and percent.isNaN()) ifNaN!!() else percent
+}
+
+fun Double.percentOf(value: Int, ifNaN: (() -> Double)? = null): Double {
+	val percent = value.div(this).times(100)
+	return if ((ifNaN != null) and percent.isNaN()) ifNaN!!() else percent
+}
+
+fun Double.roundPercentOf(value: Double): Int {
+	return value.div(this).times(100).roundToInt()
+}
+
+fun Double.roundPercentOf(value: Int): Int {
+	return value.div(this).times(100).roundToInt()
+}
+
+
+/**
  * Convert an Int pixel value to Dp.
  */
 @Composable
